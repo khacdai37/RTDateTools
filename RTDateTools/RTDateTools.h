f87a81060ee16031ecd89f407eb4e8f055924cf8
daink <daink@review-ty.com>
2022-05-07 16:22:50 +0700

    Initial commit

---
+++ RTDateTools/RTDateTools.h
@@ -0,0 +1,18 @@
+//
+//  RTDateTools.h
+//  RTDateTools
+//
+//  Created by Nguyen Khac Dai on 4/29/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RTDateTools.
+FOUNDATION_EXPORT double RTDateToolsVersionNumber;
+
+//! Project version string for RTDateTools.
+FOUNDATION_EXPORT const unsigned char RTDateToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RTDateTools/PublicHeader.h>
+
+
